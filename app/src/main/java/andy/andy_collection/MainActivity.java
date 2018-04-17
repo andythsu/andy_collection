208d2b6c2023b68561c724f5ff3666e7e24e406d
Su <Andy.Su@allstream.com>
2018-04-17 11:07:34 -0400

    added default homescreen message

--- app/src/main/java/andy/andy_collection/MainActivity.java
+++ app/src/main/java/andy/andy_collection/MainActivity.java
@@ -49,6 +49,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         parent_list_view = (ListView) findViewById(R.id.parent_list);
         db_connect_error = (TextView) findViewById(R.id.no_db_label);
+        
+        // sets the default screen message
+        db_connect_error.setVisibility(View.VISIBLE);
 
         // getting instance of Azure Mobile Client
         if(mClient == null){
